ef9b233315da929d2354d58cf337a05b99cb74ec
anmolmadaan368 <43415604918anmolmadaan@gmail.com>
2025-05-18 22:19:43 +0530

    subscribe channel

--- streamsphere/backend/controllers/profileController.js
+++ streamsphere/backend/controllers/profileController.js
@@ -117,3 +117,116 @@ exports.getMyProfile = async (req, res) => {
     res.status(500).send('Server Error');
   }
 };
+// Update profile
+exports.updateProfile = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  try {
+    const {
+      name,
+      bio,
+      location,
+      websiteUrl,
+      socialLinks
+    } = req.body;
+    
+    const profileFields = {};
+    if (name) profileFields.name = name;
+    if (bio) profileFields.bio = bio;
+    if (location) profileFields.location = location;
+    if (websiteUrl) profileFields.websiteUrl = websiteUrl;
+    
+    if (socialLinks) {
+      profileFields.socialLinks = {};
+      if (socialLinks.twitter) profileFields.socialLinks.twitter = socialLinks.twitter;
+      if (socialLinks.facebook) profileFields.socialLinks.facebook = socialLinks.facebook;
+      if (socialLinks.instagram) profileFields.socialLinks.instagram = socialLinks.instagram;
+      if (socialLinks.tiktok) profileFields.socialLinks.tiktok = socialLinks.tiktok;
+    }
+    
+    // Avatar file is handled separately by multer middleware
+    if (req.file) {
+      profileFields.avatar = `/uploads/profile/${req.file.filename}`;
+    }
+    
+    const user = await User.findByIdAndUpdate(
+      req.user.id,
+      { $set: profileFields },
+      { new: true }
+    ).select('-password');
+    
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
+
+// Subscribe to a channel
+exports.subscribe = async (req, res) => {
+  try {
+    const { channelId } = req.params;
+    const userId = req.user.id;
+    
+    // Check if channel exists
+    const channel = await User.findById(channelId);
+    if (!channel) {
+      return res.status(404).json({ message: 'Channel not found' });
+    }
+    
+    // Check if already subscribed
+    const existingSubscription = await Subscription.findOne({
+      subscriber: userId,
+      channel: channelId
+    });
+    
+    if (existingSubscription) {
+      // Unsubscribe if already subscribed
+      await existingSubscription.remove();
+      
+      // Update user's subscribedTo list
+      await User.findByIdAndUpdate(userId, {
+        $pull: { subscribedTo: channelId }
+      });
+      
+      // Update channel's subscribers list
+      await User.findByIdAndUpdate(channelId, {
+        $pull: { subscribers: userId }
+      });
+      
+      return res.json({ 
+        subscribed: false,
+        message: 'Unsubscribed from channel'
+      });
+    }
+    
+    // Create new subscription
+    const subscription = new Subscription({
+      subscriber: userId,
+      channel: channelId
+    });
+    
+    await subscription.save();
+    
+    // Update user's subscribedTo list
+    await User.findByIdAndUpdate(userId, {
+      $push: { subscribedTo: channelId }
+    });
+    
+    // Update channel's subscribers list
+    await User.findByIdAndUpdate(channelId, {
+      $push: { subscribers: userId }
+    });
+    
+    res.json({ 
+      subscribed: true,
+      message: 'Subscribed to channel'
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+}
