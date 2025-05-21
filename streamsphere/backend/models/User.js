0f0b8302e780eef33333b79711c80323c3843c15
anmolmadaan368 <43415604918anmolmadaan@gmail.com>
2025-05-21 22:02:08 +0530

    user model

--- streamsphere/backend/models/User.js
+++ streamsphere/backend/models/User.js
@@ -32,7 +32,89 @@ const userSchema = new Schema({
     isAdmin: {
         type:Boolean,
         default:false
+    },
+    bannerImage: {
+    type: String,
+    default: ''
+  },
+  bio: {
+    type: String,
+    maxlength: 1000,
+    default: ''
+  },
+  location: {
+    type: String,
+    default: ''
+  },
+  websiteUrl: {
+    type: String,
+    default: ''
+  },
+  socialLinks: {
+    twitter: { type: String, default: '' },
+    facebook: { type: String, default: '' },
+    instagram: { type: String, default: '' },
+    tiktok: { type: String, default: '' }
+  },
+  subscribers: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  subscribedTo: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  playlists: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Playlist'
+  }],
+  watchHistory: [{
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: 'Video'
+    },
+    watchedAt: {
+      type: Date,
+      default: Date.now
+    },
+    progress: {
+      type: Number,
+      default: 0 // Progress in seconds
     }
+  }],
+  likedVideos: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Video'
+  }],
+  savedVideos: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Video'
+  }],
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  channelCreatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  totalViews: {
+    type: Number,
+    default: 0
+  }
 
 },{
     timestamps:true
